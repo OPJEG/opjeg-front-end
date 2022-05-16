1b0e518887441687f8cbf17b6f3375126ce26a92
unnawut <unnawut@unnawut.com>
2022-05-17 01:08:51 +0700

    Debug module not found

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
 import type { NextPage } from 'next'
 import { Spacer } from "@nextui-org/react";
-// import SectionHead from 'components/SectionHead'
+import SectionHead from 'components/SectionHead'
 import OptionGallery from 'components/option/OptionGallery'
 
 const Home: NextPage = () => {
   return (
     <main>
-{/*      <SectionHead
+      <SectionHead
         title='Options Market'
         subtitle='Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-        />*/}
+        />
 
       <OptionGallery title="Call Options" />
       <Spacer y={3} />
