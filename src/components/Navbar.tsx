01c116f23ed627c6069290a08751404b849bb385
Nithi Nomprawat <nithi.boss157@gmail.com>
2022-06-02 00:57:43 +0700

    style: disconnect button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,53 +16,61 @@ export default function Navbar() {
   useInactiveListener(!triedEager);
 
   return (
-    <>
-      <Container fluid css={{ backdropFilter: 'blur(100px)' }}>
-        <Row css={{ alignItems: 'center', h: "$24" }}>
-          <Link href="/">
-            <Image
-              objectFit="cover"
-              src="/logo.png"
-              alt="OBJEG Logo"
-              width="200px"
-              align-items='center'
-              css={{ cursor: 'pointer' }}
-              />
+    <Container fluid css={{ backdropFilter: "blur(100px)" }} as="nav">
+      <Row css={{ alignItems: "center", h: "$24" }}>
+        <Link href="/">
+          <Image
+            objectFit="cover"
+            src="/logo.png"
+            alt="OBJEG Logo"
+            width="200px"
+            align-items="center"
+            css={{ cursor: "pointer" }}
+          />
+        </Link>
+        <Col>
+          <Link
+            href="/#"
+            underline
+            color="text"
+            css={{ m: "0 1rem", fontWeight: "bold" }}
+          >
+            Marketplace
           </Link>
-          <Col>
-              <Link href="/#" underline color="text" css={{ m: '0 1rem', fontWeight: 'bold' }}>Marketplace</Link>
-              <Link href="/#/account/options" underline color="text" css={{ m: '0 1rem', fontWeight: 'bold' }}>My Options</Link>
-              <Link href="/#/about" underline color="text" css={{ m: '0 1rem', fontWeight: 'bold' }}>About</Link>
-          </Col>
-          <Col>
-            { !active &&
-              <Button
-                ghost
-                color="secondary"
-                css={{ marginLeft: 'auto' }}
-                onPress={metamask}
-                >Connect Wallet</Button>
-            }
-            { active &&
-              <Container>
-                <Row align="center">
-                  <Col>
-                    <Text>{ account }</Text>
-                  </Col>
-                  <Spacer x={1} />
-                  <Col>
-                    <Button
-                      ghost
-                      color="secondary"
-                      onPress={deactivate}
-                      >Disconnect</Button>
-                  </Col>
-                </Row>
-              </Container>
-            }
-          </Col>
-        </Row>
-      </Container>
-    </>
-  )
+          <Link
+            href="/#/account/options"
+            underline
+            color="text"
+            css={{ m: "0 1rem", fontWeight: "bold" }}
+          >
+            My Options
+          </Link>
+          <Link
+            href="/#/about"
+            underline
+            color="text"
+            css={{ m: "0 1rem", fontWeight: "bold" }}
+          >
+            About
+          </Link>
+        </Col>
+        <Col>
+          <Row justify="flex-end">
+            {active ? (
+              <Button.Group color="secondary">
+                <Button disabled>{account && shortenAddress(account)}</Button>
+                <Button ghost onPress={deactivate}>
+                  Disconnect
+                </Button>
+              </Button.Group>
+            ) : (
+              <Button ghost color="secondary" onPress={metamask}>
+                Connect Wallet
+              </Button>
+            )}
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
